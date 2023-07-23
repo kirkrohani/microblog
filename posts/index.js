1631c5376a992bfdf8eac2ba8cdc6193ca58e30e
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-23 17:51:02 -0500

    created posts express app w/get method to get all posts and post method to create a new post

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const randomBytes = require('crypto');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id, title
+  }
+  res.send(201).send(posts[id]);
+
+});
+
+app.listen(4000, () => {
+  console.log('App - Posts: listening on port 4000');
+})
