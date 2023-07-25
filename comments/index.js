1735aac0ede1aa8707a4ee2197d8246cd34898ee
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-24 23:11:55 -0500

    updated posts, comments service to use cors npm package

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 const posts = {};
 
 app.get('/posts', (req, res) => {
