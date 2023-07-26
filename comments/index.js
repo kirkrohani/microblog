4fd9e632b721066185f142fe516355cb948e13fc
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-26 13:18:53 -0500

    added listener for events in Posts and Comments services

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,14 +14,27 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const id = crypto.randomBytes(4).toString("hex");
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({id, content});
   commentsByPostId[req.params.id] = comments;
-  res.status(201).send(comments);
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id
+    }
+  })
+  res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  console.log('Event Received by Comments: ', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -33,6 +33,11 @@ app.post('/posts', async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log('Event Received by Posts: ', req.body.type);
+  res.send({});
+});
+
 app.listen(4000, () => {
   console.log('App - Posts: listening on port 4000');
 })
