a6b59db2a2735052e5af2d7c85f7d3690da1912f
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-29 14:49:10 -0500

    updated title of create post in App updated PostList to fetch posts from QueryService updated CommentsList to used passed down prop instead of a separate query

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
+import axios from "axios";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
-
+    const res = await axios.get("http://localhost:4002/posts");
+    console.log('Fetched posts: ', res.data);
     setPosts(res.data);
   };
 
@@ -25,7 +26,7 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
