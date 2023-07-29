c57dc424f5444cb37bba05e0a68af517fbdc5349
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-28 23:58:02 -0500

    updated events in query index.js to handle a new post event or comment created event

--- query/index.js
+++ query/index.js
@@ -14,9 +14,25 @@ app.get('/posts', (req, res) => {
 
 app.post("/events", (req, res) => {
   console.log('Event Received by Query: ', req.body.type);
+    const { type, data } = req.body;
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    postsWithComments[id] = {
+      id,
+      title,
+      comments: []
+    };
+   }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = postsWithComments[id];
+    post.comments.push({ id, content });
+  }
   res.send({});
 });
 
 app.listen(4002, () => {
   console.log('App - Query: listening on port 4002');
+
 })
